2245260bbebd49f38fbcc9585b8e3a2a320a0af3
HisenZhang <993848217@qq.com>
2019-07-31 11:23:38 +0800

    fix sigsegv bug

--- System/catkin_ws/devel/setup.sh
+++ System/catkin_ws/devel/setup.sh
@@ -5,13 +5,12 @@
 # It tries it's best to undo changes from a previously sourced setup file before.
 # Supported command line options:
 # --extend: skips the undoing of changes from a previously sourced setup file
-# --local: only considers this workspace but not the chained ones
-# In plain sh shell which doesn't support arguments for sourced scripts you can
-# set the environment variable `CATKIN_SETUP_UTIL_ARGS=--extend/--local` instead.
+#   (in plain sh shell which does't support arguments for sourced scripts you
+#   can set the environment variable `CATKIN_SETUP_UTIL_ARGS=--extend` instead)
 
 # since this file is sourced either use the provided _CATKIN_SETUP_DIR
 # or fall back to the destination set at configure time
-: ${_CATKIN_SETUP_DIR:=/home/robomaster/Documents/RM2019SummerCamp/System/catkin_ws/devel}
+: ${_CATKIN_SETUP_DIR:=/home/dji/RM2019SummerCamp/System/catkin_ws/devel}
 _SETUP_UTIL="$_CATKIN_SETUP_DIR/_setup_util.py"
 unset _CATKIN_SETUP_DIR
 
--- init.sh
+++ init.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-ip link set can1 type can bitrate 1000000
-sudo ip link set up can1
+ip link set can0 type can bitrate 1000000
+sudo ip link set up can0
