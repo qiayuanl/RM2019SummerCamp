3a388c2bdb46839b561d59f067ca8c99a54b63ee
HisenZhang <993848217@qq.com>
2019-07-31 10:52:56 +0800

    Merge branch 'master' of github.com:QiayuanLiao/RM2019SummerCamp

---
+++ System/catkin_ws/build_isolated/openworld/CMakeFiles/teleop.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/teleop.dir/src/opmode/teleop.cpp.o"
+  "/home/qiayuanliao/Project/RM2019SummerCamp/System/catkin_ws/devel_isolated/openworld/lib/openworld/teleop.pdb"
+  "/home/qiayuanliao/Project/RM2019SummerCamp/System/catkin_ws/devel_isolated/openworld/lib/openworld/teleop"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/teleop.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ System/catkin_ws/devel_isolated/env.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env sh
+# generated from catkin.builder Python module
+
+/home/qiayuanliao/Project/RM2019SummerCamp/System/catkin_ws/devel_isolated/usb_cam/env.sh "$@"
