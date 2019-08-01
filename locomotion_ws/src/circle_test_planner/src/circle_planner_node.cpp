b55d7547bf9b746be5ea03f7b318400c4a92de20
imoneoi <imone@tuta.io>
2019-08-01 21:50:10 +0800

    Circle Task OK

--- locomotion_ws/src/circle_test_planner/src/circle_planner_node.cpp
+++ locomotion_ws/src/circle_test_planner/src/circle_planner_node.cpp
@@ -4,30 +4,30 @@
 #include <geometry_msgs/Twist.h>
 #include <cmath>
 double x, y, theta;
-double r = 0.5;
-double w = 180;
+double r = 0.8;
+double w = 45;
 
 double t;
 
-geometry_msgs::Pose plan;
+geometry_msgs::PoseStamped plan;
 tf::Quaternion plan_q;
 int main(int argc, char **argv) {
   ros::init(argc, argv, "base_update_node");
 
   ros::NodeHandle nh;
-  ros::Publisher planner_pub = nh.advertise<geometry_msgs::Pose>("setpose", 1000);
+  ros::Publisher planner_pub = nh.advertise<geometry_msgs::PoseStamped>("move_base_simple/goal", 100);
   ros::Rate rate(100);
   while (ros::ok()) {
     x = cos(0.01745 * w * t) * r;
     y = sin(0.01745 * w * t) * r;
-    theta = w * t + 3.1415;
+    theta = 0.01745*(w * t) + 3.1415926;
     plan_q.setRPY(0, 0, theta);
-    plan.orientation.x = plan_q.getX();
-    plan.orientation.y = plan_q.getY();
-    plan.orientation.z = plan_q.getZ();
-    plan.orientation.w = plan_q.getW();
-    plan.position.x = x;
-    plan.position.y = y;
+    plan.pose.orientation.x = plan_q.getX();
+    plan.pose.orientation.y = plan_q.getY();
+    plan.pose.orientation.z = plan_q.getZ();
+    plan.pose.orientation.w = plan_q.getW();
+    plan.pose.position.x = x;
+    plan.pose.position.y = y;
     planner_pub.publish(plan);
     t += 0.01;
     rate.sleep();
