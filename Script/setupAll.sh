a8dfa13cd37c5b806769e1ec14caaed82857b2e4
HisenZhang <993848217@qq.com>
2019-08-04 14:26:24 +0800

    parallel order conflict solved; rename init.sh -> setupCAN.sh

--- Script/setupAll.sh
+++ Script/setupAll.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
 sudo ~/jetson_clocks.sh
-sudo ~/RM2019SummerCamp/Script/init.sh
-roscore # &
-# rosrun rcbigcar robot &
-# roslaunch rcbigvis rcbigvis.launch &
+echo "\n\n\n ================= FULL POWER ENABLED ================= \n\n\n"
+
+sudo ~/RM2019SummerCamp/Script/setupCAN.sh
+echo "\n\n\n ================= CAN INITIALIZED ================= \n\n\n"
+
+roscore &
+sleep 10
+echo "\n\n\n ================= ROSCORE STARTED ================= \n\n\n"
+
+rosrun rcbigcar robot &
+echo "\n\n\n ================= MOTION ENABLED ================= \n\n\n"
+
+roslaunch rcbigvis rcbigvis.launch &
+echo "\n\n\n ================= VISION ENABLED ================= \n\n\n"
