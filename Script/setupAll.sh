b44fd12ad27de98aac4705900db8d76b04958664
HisenZhang <993848217@qq.com>
2019-08-04 14:18:30 +0800

    ban rosrun & roslaunch

--- Script/setupAll.sh
+++ Script/setupAll.sh
@@ -1,8 +1,7 @@
-sudo /home/dji/jetson_clocks.sh &
-sudo bash /home/qiayuanliao/RM2019SummerCamp/Script/init.sh &
-roscore &
-rosrun rcbigcar robot &
-roslaunch rcbigvis rcbigvis.launch &
-# rosrun rcbigctl controller &
+#!/bin/bash
 
-
+sudo ~/jetson_clocks.sh
+sudo ~/RM2019SummerCamp/Script/init.sh
+roscore # &
+# rosrun rcbigcar robot &
+# roslaunch rcbigvis rcbigvis.launch &
