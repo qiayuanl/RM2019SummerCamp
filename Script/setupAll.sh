626dd4d671bc3c6c1c8db7fc34add4f44d86d642
HisenZhang <993848217@qq.com>
2019-08-04 13:44:25 +0800

    startup scripts

--- Script/setupAll.sh
+++ Script/setupAll.sh
@@ -1,7 +1,8 @@
-sudo /home/dji/jetson_clocks.sh
-sudo bash /home/qiayuanliao/RM2019SummerCamp/Script/init.sh
-rosrun rcbigcar robot
-roslaunch rcbigvis rcbigvis.launch
-rosrun rcbigctl controller
-roscore
+sudo /home/dji/jetson_clocks.sh &
+sudo bash /home/qiayuanliao/RM2019SummerCamp/Script/init.sh &
+roscore &
+rosrun rcbigcar robot &
+roslaunch rcbigvis rcbigvis.launch &
+# rosrun rcbigctl controller &
+
 
