5fc7c8a92b8798b46992bf2cdc275a6326c00299
QiayuanLiao <liaoqiayuan@gmail.com>
2019-08-05 09:27:57 +0800

    add simple_controller

---
+++ locomotion_ws/src/simple_controller/src/controller.cpp
@@ -0,0 +1,39 @@
+
+#include <cmath>
+#include <algorithm>
+
+#include <ros/ros.h>
+#include <tf/tf.h>
+#include <tf/transform_listener.h>
+
+#include <geometry_msgs/Twist.h>
+#include <geometry_msgs/Pose.h>
+#include <nav_msgs/Odometry.h>
+#include <nav_msgs/Path.h>
+#include <std_msgs/Float64MultiArray.h>
+
+#include <dynamic_reconfigure/server.h>
+#include "simple_controller/DynReconf1.h"
+
+
+int main(int argc, char **argv)
+{
+	ros::init(argc, argv, "controller");
+
+	ros::NodeHandle nh;
+
+	//Create Nodes
+	PositionCtl positionctl;
+
+	ros::Rate loop_rate(100);
+
+	//Process Jobs
+	while (ros::ok())
+	{
+		ros::spinOnce();
+		loop_rate.sleep();
+	}
+
+	//Release Nodes
+	return 0;
+}
