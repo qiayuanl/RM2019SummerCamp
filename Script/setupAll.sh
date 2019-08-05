39526b9e663f5e6713c2f85da5129d76af08a0dc
imoneoi <imone@tuta.io>
2019-08-05 10:11:49 +0800

    modified setup script

--- Script/setupAll.sh
+++ Script/setupAll.sh
@@ -1,17 +1,11 @@
 #!/bin/bash
 
 sudo ~/jetson_clocks.sh
-echo "\n\n\n ================= FULL POWER ENABLED ================= \n\n\n"
+echo "================= FULL POWER ENABLED ================="
+sleep 1
 
 sudo ~/RM2019SummerCamp/Script/setupCAN.sh
-echo "\n\n\n ================= CAN INITIALIZED ================= \n\n\n"
+echo "================= CAN INITIALIZED ================="
+sleep 1
 
-roscore &
-sleep 10
-echo "\n\n\n ================= ROSCORE STARTED PID: $! ================= \n\n\n"
-
-rosrun rcbigcar robot &
-echo "\n\n\n ================= MOTION ENABLED PID: $! ================= \n\n\n"
-
-roslaunch rcbigvis rcbigvis.launch &
-echo "\n\n\n ================= VISION ENABLED PID: $! ================= \n\n\n"
+roslaunch ~/RM2019SummerCamp/Script/setupNodes.launch
