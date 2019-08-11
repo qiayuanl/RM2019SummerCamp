845032efa162448861588303a8cdb56f68d29bbc
HisenZhang <993848217@qq.com>
2019-08-11 10:02:22 +0800

    multi motor sync

--- RCBigController/catkin_ws/src/rcbigctl/src/opmode/testpid.cpp
+++ RCBigController/catkin_ws/src/rcbigctl/src/opmode/testpid.cpp
@@ -13,22 +13,18 @@ int main(int argc, char **argv)
 	ros::Rate loop_rate(50);
 	ros::Time t = ros::Time::now();
 
-	double value = -8.0 * M_PI;
 	bool is_on = true;
 
 	data.data.resize(4);
-	for (int i = 0; i < data.data.size(); i++)
-		data.data[i] = value;
-
 	while (ros::ok())
 	{
-		if ((ros::Time::now() - t).toSec() >= 5.0)
+		if ((ros::Time::now() - t).toSec() >= 0.7)
 		{
 			t = ros::Time::now();
 
 			is_on = !is_on;
 			for (int i = 0; i < data.data.size(); i++)
-				data.data[i] = is_on ? value : 0;
+				data.data[i] = is_on ? -1.1 : -1.9;
 		}
 
 		data_pub.publish(data);
